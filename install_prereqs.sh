6bf3c85ddc416f1af99da40d553d68b82abd11e8
Caio Yuri da Silva Costa <caio@debian-slidepad-vm.grupo.local>
2017-11-16 16:30:30 -0200

    update

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sudo apt install build-essential autoconf libtool make openjdk-8-jdk
+sudo apt install build-essential autoconf libtool make openjdk-8-jdk python-setuptools
+cd $HOME
+wget https://redirector.gvt1.com/edgedl/go/go1.6.3.linux-amd64.tar.gz
+tar -zvxf go1.6.3.linux-amd64.tar.gz
+export GOROOT=$HOME/go
+export PATH=$PATH:$GOROOT/bin
+
+
 
 
