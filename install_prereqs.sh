56c71125dacce597d4d74ad9ce47248231a699aa
caioycosta <caioycosta@github>
2017-11-27 20:26:36 -0200

    versao nova de go

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 sudo apt install build-essential autoconf libtool make openjdk-8-jdk python-setuptools
-cd $HOME
-wget https://redirector.gvt1.com/edgedl/go/go1.6.3.linux-amd64.tar.gz
-tar -zvxf go1.6.3.linux-amd64.tar.gz
-export GOROOT=$HOME/go
-export PATH=$PATH:$GOROOT/bin
+pushd $HOME
+if [ ! -d go192 ]
+then
+	if [ ! -f go1.9.2.linux-amd64.tar.gz ]
+	then
+		wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
+	fi
+	mkdir go192
+	pushd go192
+	tar -zvxf ../go1.9.2.linux-amd64.tar.gz
+	popd
+	mv go192/go/* go192/
+fi
+export GOROOT=$HOME/go192
+export PATH=$GOROOT/bin:$PATH
+popd
 
 
 
